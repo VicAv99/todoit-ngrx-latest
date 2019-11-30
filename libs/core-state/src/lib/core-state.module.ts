46efddde57018c957d489d9888010f16d98acb96
Victor Avila <victor.avila@venturplex.com>
2019-11-30 12:34:37 -0700

    add: ngrx main module

--- libs/core-state/src/lib/core-state.module.ts
+++ libs/core-state/src/lib/core-state.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NxModule } from '@nrwl/nx';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule, RootStoreConfig } from '@ngrx/store';
+
+const storeConfig: RootStoreConfig<any> = {
+  runtimeChecks: {
+    strictActionImmutability: true,
+    strictStateImmutability: true
+  }
+}
+
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    NxModule.forRoot(),
+    StoreModule.forRoot({}, storeConfig),
+    StoreDevtoolsModule.instrument({ maxAge: 10 }),
+    EffectsModule.forRoot([])
+  ]
 })
 export class CoreStateModule {}
